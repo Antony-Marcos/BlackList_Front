9b3fcc31f7c44d5cb3f7aa85d5549db4bb4bd5b8
Antony <makmenezes2@gmail.com>
2025-06-07 22:10:33 -0300

    ataulizações do import

--- src/Registro.jsx
+++ src/Registro.jsx
@@ -64,7 +64,7 @@ export default function Registro() {
           <div className='relative'>
           <input
             type={mpassword ? 'password': 'text'}
-            placeholder="Insira a senha"
+            placeholder="Senha"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="w-full px-4 py-2 mb-4 bg-gray-800 border border-gray-600 rounded"
@@ -108,7 +108,7 @@ export default function Registro() {
           </button>
         </form>
 
-        <p className="mt-4 text-sm text-gray-400">
+        <p className="mt-4 text-sm text-gray-400  text-center">
           Já tem conta?{' '}
           <Link to="/" className="text-red-400 hover:underline">
             Login
